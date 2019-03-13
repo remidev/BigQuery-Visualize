638247a97b37899de90c473d0db867e00a25b8f4
Pinyin <kevin101zhang@gmail.com>
2019-03-12 21:50:17 -0400

    NPM INSTALL DEPENCIES Have to set up any databases

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"); // Going to have to change this too to a local Mongo database
 
 // Start the API server
 app.listen(PORT, function() {
