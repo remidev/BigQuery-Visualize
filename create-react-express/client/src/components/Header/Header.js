9085322eba969357aa1e7fe950b7f231b8a0beca
Pinyin <kevin101zhang@gmail.com>
2019-03-12 21:46:17 -0400

    Intial AGAIN

---
+++ create-react-express/client/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1 className="text-center">Web Scrape With Us!</h1>
+    </div>
+  );
+}
+export default Header;
---
+++ create-react-express/controllers/webController.js
@@ -0,0 +1,6 @@
+const db = require("../models");
+
+// Defining methods for the WebController
+module.exports = {
+
+}
