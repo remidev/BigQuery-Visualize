7b7fe4db7be391e319c0315d144b98971e91a8a1
Pinyin <kevin101zhang@gmail.com>
2019-03-14 21:52:08 -0400

    semantics Lib

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
+import 'semantic-ui-css/semantic.min.css';
 
 import Header from "./components/Header/Header";
+import DropdownSource from "./components/DropdownSource/DropdownSource";
 
 class App extends Component {
   render() {
     return (
   <div>
     <Header/>
+<DropdownSource/>
+      
+
+
   </div>
     );
   }
