f9c70b5677db926bc09888fe8940944aa9f7959b
Pinyin <kevin101zhang@gmail.com>
2019-03-16 18:05:19 -0400

    Experimenting with d3

---
+++ create-react-express/client/src/TestingJsonFile.js
@@ -0,0 +1,403 @@
+var TestingJsonFile = [
+    {
+    id: 1,
+    nm: "Edward the Elder",
+    cty: "United Kingdom",
+    hse: "House of Wessex",
+    yrs: 822
+    },
+    {
+    id: 2,
+    nm: "Athelstan",
+    cty: "United Kingdom",
+    hse: "House of Wessex",
+    yrs: 952
+    },
+    {
+    id: 3,
+    nm: "Edmund",
+    cty: "United Kingdom",
+    hse: "House of Wessex",
+    yrs: 940
+    },
+    {
+    id: 4,
+    nm: "Edred",
+    cty: "United Kingdom",
+    hse: "House of Wessex",
+    yrs: 946
+    },
+    {
+    id: 5,
+    nm: "Edwy",
+    cty: "United Kingdom",
+    hse: "House of Wessex",
+    yrs: 955
+    },
+    {
+    id: 6,
+    nm: "Edgar",
+    cty: "United Kingdom",
+    hse: "House of Wessex",
+    yrs: 959
+    },
+    {
+    id: 7,
+    nm: "Edward the Martyr",
+    cty: "United Kingdom",
+    hse: "House of Wessex",
+    yrs: 975
+    },
+    {
+    id: 8,
+    nm: "Ethelred II the Unready",
+    cty: "United Kingdom",
+    hse: "House of Wessex",
+    yrs: 978
+    },
+    {
+    id: 9,
+    nm: "Edmund lronside",
+    cty: "United Kingdom",
+    hse: "House of Wessex",
+    yrs: 101
+    },
+    {
+    id: 10,
+    nm: "Cnut",
+    cty: "United Kingdom",
+    hse: "House of Denmark",
+    yrs: 101
+    },
+    {
+    id: 11,
+    nm: "Harold I Harefoot",
+    cty: "United Kingdom",
+    hse: "House of Denmark",
+    yrs: 103
+    },
+    {
+    id: 12,
+    nm: "Harthacanut",
+    cty: "United Kingdom",
+    hse: "House of Denmark",
+    yrs: 104
+    },
+    {
+    id: 13,
+    nm: "Edward the Confessor",
+    cty: "United Kingdom",
+    hse: "House of Wessex",
+    yrs: 104
+    },
+    {
+    id: 14,
+    nm: "Harold II",
+    cty: "United Kingdom",
+    hse: "House of Wessex",
+    yrs: 106
+    },
+    {
+    id: 15,
+    nm: "William I",
+    cty: "United Kingdom",
+    hse: "House of Normandy",
+    yrs: 106
+    },
+    {
+    id: 16,
+    nm: "William II",
+    cty: "United Kingdom",
+    hse: "House of Normandy",
+    yrs: 108
+    },
+    {
+    id: 17,
+    nm: "Henry I",
+    cty: "United Kingdom",
+    hse: "House of Normandy",
+    yrs: 110
+    },
+    {
+    id: 18,
+    nm: "Stephen",
+    cty: "United Kingdom",
+    hse: "House of Blois",
+    yrs: 113
+    },
+    {
+    id: 19,
+    nm: "Henry II",
+    cty: "United Kingdom",
+    hse: "House of Angevin",
+    yrs: 115
+    },
+    {
+    id: 20,
+    nm: "Richard I",
+    cty: "United Kingdom",
+    hse: "House of Angevin",
+    yrs: 118
+    },
+    {
+    id: 21,
+    nm: "John",
+    cty: "United Kingdom",
+    hse: "House of Angevin",
+    yrs: 119
+    },
+    {
+    id: 22,
+    nm: "Henry III",
+    cty: "United Kingdom",
+    hse: "House of Plantagenet",
+    yrs: 121
+    },
+    {
+    id: 23,
+    nm: "Edward I",
+    cty: "United Kingdom",
+    hse: "House of Plantagenet",
+    yrs: 127
+    },
+    {
+    id: 24,
+    nm: "Edward II",
+    cty: "United Kingdom",
+    hse: "House of Plantagenet",
+    yrs: 130
+    },
+    {
+    id: 25,
+    nm: "Edward III",
+    cty: "United Kingdom",
+    hse: "House of Plantagenet",
+    yrs: 132
+    },
+    {
+    id: 26,
+    nm: "Richard II",
+    cty: "United Kingdom",
+    hse: "House of Plantagenet",
+    yrs: 137
+    },
+    {
+    id: 27,
+    nm: "Henry IV",
+    cty: "United Kingdom",
+    hse: "House of Lancaster",
+    yrs: 139
+    },
+    {
+    id: 28,
+    nm: "Henry V",
+    cty: "United Kingdom",
+    hse: "House of Lancaster",
+    yrs: 141
+    },
+    {
+    id: 29,
+    nm: "Henry VI",
+    cty: "United Kingdom",
+    hse: "House of Lancaster",
+    yrs: 142
+    },
+    {
+    id: 30,
+    nm: "Edward IV",
+    cty: "United Kingdom",
+    hse: "House of York",
+    yrs: 146
+    },
+    {
+    id: 31,
+    nm: "Edward V",
+    cty: "United Kingdom",
+    hse: "House of York",
+    yrs: 148
+    },
+    {
+    id: 32,
+    nm: "Richard III",
+    cty: "United Kingdom",
+    hse: "House of York",
+    yrs: 148
+    },
+    {
+    id: 33,
+    nm: "Henry VII",
+    cty: "United Kingdom",
+    hse: "House of Tudor",
+    yrs: 148
+    },
+    {
+    id: 34,
+    nm: "Henry VIII",
+    cty: "United Kingdom",
+    hse: "House of Tudor",
+    yrs: 150
+    },
+    {
+    id: 35,
+    nm: "Edward VI",
+    cty: "United Kingdom",
+    hse: "House of Tudor",
+    yrs: 154
+    },
+    {
+    id: 36,
+    nm: "Mary I",
+    cty: "United Kingdom",
+    hse: "House of Tudor",
+    yrs: 155
+    },
+    {
+    id: 37,
+    nm: "Elizabeth I",
+    cty: "United Kingdom",
+    hse: "House of Tudor",
+    yrs: 155
+    },
+    {
+    id: 38,
+    nm: "James I",
+    cty: "United Kingdom",
+    hse: "House of Stuart",
+    yrs: 160
+    },
+    {
+    id: 39,
+    nm: "Charles I",
+    cty: "United Kingdom",
+    hse: "House of Stuart",
+    yrs: 162
+    },
+    {
+    id: 40,
+    nm: "Commonwealth",
+    cty: "United Kingdom",
+    hse: "Commonwealth",
+    yrs: 164
+    },
+    {
+    id: 41,
+    nm: "Oliver Cromwell",
+    cty: "United Kingdom",
+    hse: "Commonwealth",
+    yrs: 165
+    },
+    {
+    id: 42,
+    nm: "Richard Cromwell",
+    cty: "United Kingdom",
+    hse: "Commonwealth",
+    yrs: 165
+    },
+    {
+    id: 43,
+    nm: "Charles II",
+    cty: "United Kingdom",
+    hse: "House of Stuart",
+    yrs: 166
+    },
+    {
+    id: 44,
+    nm: "James II",
+    cty: "United Kingdom",
+    hse: "House of Stuart",
+    yrs: 168
+    },
+    {
+    id: 45,
+    nm: "William III",
+    cty: "United Kingdom",
+    hse: "House of Orange",
+    yrs: 168
+    },
+    {
+    id: 46,
+    nm: "Anne",
+    cty: "United Kingdom",
+    hse: "House of Stuart",
+    yrs: 170
+    },
+    {
+    id: 47,
+    nm: "George I",
+    cty: "United Kingdom",
+    hse: "House of Hanover",
+    yrs: 171
+    },
+    {
+    id: 48,
+    nm: "George II",
+    cty: "United Kingdom",
+    hse: "House of Hanover",
+    yrs: 172
+    },
+    {
+    id: 49,
+    nm: "George III",
+    cty: "United Kingdom",
+    hse: "House of Hanover",
+    yrs: 176
+    },
+    {
+    id: 50,
+    nm: "George IV",
+    cty: "United Kingdom",
+    hse: "House of Hanover",
+    yrs: 182
+    },
+    {
+    id: 51,
+    nm: "William IV",
+    cty: "United Kingdom",
+    hse: "House of Hanover",
+    yrs: 183
+    },
+    {
+    id: 52,
+    nm: "Victoria",
+    cty: "United Kingdom",
+    hse: "House of Hanover",
+    yrs: 183
+    },
+    {
+    id: 53,
+    nm: "Edward VII",
+    cty: "United Kingdom",
+    hse: "House of Saxerga",
+    yrs: 190
+    },
+    {
+    id: 54,
+    nm: "George V",
+    cty: "United Kingdom",
+    hse: "House of Windsor",
+    yrs: 191
+    },
+    {
+    id: 55,
+    nm: "Edward VIII",
+    cty: "United Kingdom",
+    hse: "House of Windsor",
+    yrs: 193
+    },
+    {
+    id: 56,
+    nm: "George VI",
+    cty: "United Kingdom",
+    hse: "House of Windsor",
+    yrs: 193
+    },
+    {
+    id: 57,
+    nm: "Elizabeth II",
+    cty: "United Kingdom",
+    hse: "House of Windsor",
+    yrs: 195
+      }
+    ]
+
+    export default TestingJsonFile;
--- create-react-express/client/src/components/Pages/Home/Home.js
+++ create-react-express/client/src/components/Pages/Home/Home.js
@@ -1,12 +1,108 @@
 import React, { Component } from "react";
-import topojson from "topojson";
+import _ from "lodash";
+// import topojson from "topojson";
 import * as d3 from "d3";
+import chroma from 'chroma-js';
+import TestingJsonFile from "../../../TestingJsonFile"
+
+var divStyle={
+  // marginLeft: "auto",
+  // marginRight: "auto",
+  // width:"40%"
+}
+var styles={
+backgroundColor:'lightgrey',
+}
+
+var width = 900;
+var height = 700;
+var radius = 15;
+
+
+//d3 functions
+
+var colorScale = chroma.scale(['red','yellow','green']);
+var amountScale = d3.scaleLog();
+
+var simulation = d3.forceSimulation()
+  .force('center', d3.forceCenter(width /2, height/2))
+  .force('charge', d3.forceManyBody())
+  .force('collide', d3.forceCollide(radius));
 
 class Home extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      Datafile: [],
+    }
+
+    this.forceTick = this.forceTick.bind(this);
+    simulation.on('tick', this.forceTick);
+  }
+
+  componentWillMount(){
+
+    var Datafile = _.chain(TestingJsonFile)
+    .filter(d => d).map(d=>{
+      return {
+        name: d.nm,
+        city: d.cty,
+        house: d.hse,
+        years: d.yrs
+      }
+    }).value();
+
+    var amountExtent = d3.extent(Datafile, d=> d.years);
+    // console.log(amountExtent);
+    amountScale.domain(amountExtent);
+
+    console.log(amountScale(100));
+   this.setState({Datafile})
+  }
+
+  componentDidMount(){
+    this.container = d3.select(this.refs.container);
+    console.log(this.refs.container, this.container);
+    this.renderCircles();
+
+    simulation.nodes(this.state.Datafile).alpha(0.4).restart();
+  } 
+
+  componentDidUpdate(){
+    this.renderCircles();
+  }
+
+  renderCircles(){
+    this.circles = this.container.selectAll('circle')
+    .data(this.state.Datafile, d=> d.name);
+
+    this.circles.exit().remove();
+
+    this.circles = this.circles.enter().append('circle')
+    .merge(this.circles)
+    .attr('r',radius)
+    .attr('fill-opacity', 0.4) 
+    .attr('fill', d => colorScale(amountScale(d.years))) 
+    .attr('stroke', d => colorScale(amountScale(d.years)));
+  }
+
+  forceTick(){
+    console.log(this.circles.datum().x, this.circles.datum().y);
+    this.circles.attr('cx', d=> d.x)
+    this.circles.attr('cy', d=>d.y)
+    .attr('r', radius);
+  }
+
   render(){
    return (
-     null
-   )
+     <div style={divStyle}>
+     <svg style={styles} width={width} height={height} ref="container">
+
+     </svg>
+     </div>
+   );
   }
 }
 
