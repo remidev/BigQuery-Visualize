2ae2bc73871e4cdabc2bfeeb41da980f40df0695
remidev <28935481+remidev@users.noreply.github.com>
2019-03-27 16:57:19 -0400

    config

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -6,7 +6,6 @@
 // Require Dependencies
 //========================================================================
 const express = require("express");
-const mongoose = require("mongoose");
 const routes = require("./routes");
 const { BigQuery } = require("@google-cloud/bigquery");
 //========================================================================
@@ -21,8 +20,12 @@ const app = express();
 const PORT = process.env.PORT || process.argv[2] || 8080;
 //========================================================================
 
-// Instantiate Google BigQuery Client
+// Instantiate Google BigQuery Client & Credentials
 //========================================================================
+const path = "";
+
+process.env.GOOGLE_APPLICATION_CREDENTIALS = path;
+
 const bigquery = new BigQuery();
 //========================================================================
 
@@ -34,33 +37,6 @@ if (process.env.NODE_ENV === "production") {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
-//========================================================================
-
-// Mongoose Connection
-//========================================================================
-const URI = process.env.MONGODB_URI || "mongodb://localhost/database";
-
-const options = {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-};
-
-// Attempt Connection
-mongoose.connect(URI, options).then(
-  result => {
-    // Connection Success
-    console.log(
-      `Connected to database '${result.connections[0].name}' on ${
-        result.connections[0].host
-      }:${result.connections[0].port}`
-    );
-  },
-  err => {
-    // Connection Fail
-    console.log("There was an error with your connection:", err);
-  }
-);
 //========================================================================
 
 // Express Connection
@@ -79,7 +55,20 @@ app.listen(PORT, function() {
 //============================
 async function query() {
   // SQL Query String
-  const sqlQuery = `#Query-Goes-Here`;
+  //const sqlQuery = `#Query-Goes-Here`;
+  const sqlQuery = `
+SELECT
+  CONCAT( 'https://stackoverflow.com/questions/', CAST(id AS STRING) ) AS url,
+  view_count,
+  creation_date
+FROM
+  \`bigquery-public-data.stackoverflow.posts_questions\`
+WHERE
+  tags LIKE '%google-bigquery%'
+ORDER BY
+  view_count DESC
+LIMIT
+  10`;
   // Query Options,
   // See link for list of options:
 
@@ -92,16 +81,17 @@ async function query() {
   const [rows] = await bigquery.query(options);
 
   console.log("Query Results:");
-  rows.forEach(row => {
-    // Accessing the data within the rows
-    const url = row["url"];
-    const viewCount = row["view_count"];
-    console.log(`url: ${url}, ${viewCount} views`);
-  });
+  // rows.forEach(row => {
+  //   // Accessing the data within the rows
+  //   const url = row["url"];
+  //   const viewCount = row["view_count"];
+  //   console.log(`url: ${url}, ${viewCount} views`);
+  // });
+  console.log(rows);
 }
 
 // Query function call
-//query().catch(console.error);
+query().catch(console.error);
 
 // __BigQuery Code Snippets__
 //========================================================================
@@ -133,3 +123,31 @@ LIMIT
 //========================================================================
 // NOTE!: un-comment this line to log big query response data
 query(...process.argv.slice(2)).catch(console.error);
+//========================================================================
+
+// Mongoose Connection
+//========================================================================
+// const URI = process.env.MONGODB_URI || "mongodb://localhost/database";
+
+// const options = {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false
+// };
+
+// // Attempt Connection
+// mongoose.connect(URI, options).then(
+//   result => {
+//     // Connection Success
+//     console.log(
+//       `Connected to database '${result.connections[0].name}' on ${
+//         result.connections[0].host
+//       }:${result.connections[0].port}`
+//     );
+//   },
+//   err => {
+//     // Connection Fail
+//     console.log("There was an error with your connection:", err);
+//   }
+// );
+//========================================================================
