208603b5284e101247cb4b4c6f2ef020be359220
Pinyin <kevin101zhang@gmail.com>
2019-04-02 05:01:17 -0400

    Search Page Minor CSS done 5:00AM

--- create-react-express/client/src/components/GalleryFile/GalleryFile.js
+++ create-react-express/client/src/components/GalleryFile/GalleryFile.js
@@ -11,8 +11,8 @@ function GalleryFile(props) {
   
   const styles = {
     listStyleType: "none",
-    border: "1px solid black",
-    borderCollapse: "collapse",
+    border: "3px solid #f0f0f1",
+    borderRadius:"3%",
   };
 
  var Sample1Title ="";
@@ -27,7 +27,7 @@ function GalleryFile(props) {
 
   return (
     <div>
-     <Segment style={{ padding: "0em", height:"100vh", width:"100vw", overflow:"hidden"}} vertical>
+     <Segment style={{ padding: "0em", overflow:"auto"}} vertical>
       <Grid celled="internally" columns="equal" stackable>
         <Grid.Row textAlign="center">
           <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em", overflow:"hidden" }}>
@@ -35,68 +35,56 @@ function GalleryFile(props) {
           </Grid.Column>
           <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
           
-          <h1>Your Query Information!</h1>
-        <div>
-          <Header>JSON FILE RECIEVED:</Header> 
-        {props.fileData.map(i => {
-        //  console.log(i);
-         return (<li style={styles}>{JSON.stringify(i)}</li>)
-        })}</div>
-        <div> 
-        <br></br>
-
-        <h1>Statistics</h1>
-        <br></br>
+          <h1>Statistics - <small>Your Sample Info</small></h1>
 
         {props.fileData.map(i => {
          ArrContent1.push(parseInt([Object.values(i)[1]]));
          ArrContent2.push(parseInt([Object.values(i)[2]]));
          Sample1Title = [Object.keys(i)[1]];
          Sample2Title = [Object.keys(i)[2]];
-        })}</div>
-        <div>
-        <h5>Sample Info</h5>
-        
+        })}
+
+      <div>
+        <div style={styles}>
         <small>{`Name: ${Sample1Title}`}</small><br></br>
         <small>{`Sorted Sample: [${ArrContent1.sort()}]`}</small><br></br>
         <small>{`DataLength: ${ArrContent1.length}`}</small><br></br>
-        <small>{`Minimum: ${ss.min(ArrContent1)}`}</small><br></br>
-        <small>{`Maximum: ${ss.max(ArrContent1)}`}</small><br></br>
-        <small>{`Sum: ${ss.sum(ArrContent1)}`}</small><br></br>
-        <small>{`Product: ${ss.product(ArrContent1)}`}</small><br></br>
-        <small>{`Mean: ${ss.mean(ArrContent1)}`}</small><br></br>
-        {/* <small>{`Harmonic Mean: ${ss.harmonicMean(Math.abs(ArrContent1)).toFixed(2)}`}</small><br></br> */}
-        <small>{`Mode: ${ss.modeFast(ArrContent1)}`}</small><br></br>
-        <small>{`Median: ${ss.median(ArrContent1)}`}</small><br></br>
-        <small>{`Skewness: ${ss.sampleSkewness(ArrContent1)}`}</small><br></br>
-        <small>{`Sample Variance: ${ss.sampleVariance(ArrContent1)}`}</small><br></br>
-        <small>{`Sample Standard Deviation: ${ss.sampleStandardDeviation(ArrContent1)}`}</small><br></br>
-        <small>{`Median Absolute Deviation: ${ss.medianAbsoluteDeviation(ArrContent1)}`}</small><br></br>
+        <small>{`Minimum: ${ss.min(ArrContent1)} | Maximum: ${ss.max(ArrContent1)}`}</small><br></br>
+        <small>{`Sum: ${ss.sum(ArrContent1)} | Product: ${ss.product(ArrContent1)}`}</small><br></br>
+        <small>{`Mean: ${ss.mean(ArrContent1)} | Mode: ${ss.modeFast(ArrContent1)} | Median: ${ss.median(ArrContent1)}`}</small><br></br>
         <small>{`InterquartileRange: ${ss.interquartileRange(ArrContent1)}`}</small><br></br>
+        <small>{`Sample Variance: ${ss.sampleVariance(ArrContent1)} | Sample Standard Deviation: ${ss.sampleStandardDeviation(ArrContent1)}`}</small><br></br>
+        <small>{`Skewness: ${ss.sampleSkewness(ArrContent1)}`}</small><br></br>
+        <small>{`Median Absolute Deviation: ${ss.medianAbsoluteDeviation(ArrContent1)}`}</small><br></br>
         <hr></hr>
 
         <small>{`Name: ${Sample2Title}`}</small><br></br>
         <small>{`Sorted Sample: [${ArrContent2.sort()}]`}</small><br></br>
         <small>{`DataLength:${ArrContent2.length}`}</small><br></br>
-        <small>{`Minimum: ${ss.min(ArrContent2)}`}</small><br></br>
-        <small>{`Maximum: ${ss.max(ArrContent2)}`}</small><br></br>
-        <small>{`Sum: ${ss.sum(ArrContent2)}`}</small><br></br>
-        <small>{`Product: ${ss.product(ArrContent2)}`}</small><br></br>
-        <small>{`Mean: ${ss.mean(ArrContent2)}`}</small><br></br>
-        {/* <small>{`Harmonic Mean: ${ss.harmonicMean(Math.abs(ArrContent2)).toFixed(2)}`}</small><br></br> */}
-        <small>{`Mode: ${ss.modeFast(ArrContent2)}`}</small><br></br>
-        <small>{`Median: ${ss.median(ArrContent2)}`}</small><br></br>
-        <small>{`Skewness: ${ss.sampleSkewness(ArrContent2)}`}</small><br></br>
-        <small>{`Sample Variance: ${ss.sampleVariance(ArrContent2)}`}</small><br></br>
-        <small>{`Sample Standard Deviation: ${ss.sampleStandardDeviation(ArrContent2)}`}</small><br></br>
-        <small>{`Median Absolute Deviation: ${ss.medianAbsoluteDeviation(ArrContent2)}`}</small><br></br>
+        <small>{`Minimum: ${ss.min(ArrContent2)} | Maximum: ${ss.max(ArrContent2)}`}</small><br></br>
+        <small>{`Sum: ${ss.sum(ArrContent2)} | Product: ${ss.product(ArrContent2)}`}</small><br></br>
+        <small>{`Mean: ${ss.mean(ArrContent2)} | Mode: ${ss.modeFast(ArrContent2)} | Median: ${ss.median(ArrContent2)}`}</small><br></br>
         <small>{`InterquartileRange: ${ss.interquartileRange(ArrContent2)}`}</small><br></br>
+        <small>{`Sample Variance: ${ss.sampleVariance(ArrContent2)} | Sample Standard Deviation: ${ss.sampleStandardDeviation(ArrContent2)}`}</small><br></br>
+        <small>{`Skewness: ${ss.sampleSkewness(ArrContent2)}`}</small><br></br>
+        <small>{`Median Absolute Deviation: ${ss.medianAbsoluteDeviation(ArrContent2)}`}</small><br></br>
         <hr></hr>
 
         <h6>Compare the Datasets!</h6>
         <small>{`Sample Correlation ${ss.sampleCorrelation(ArrContent1, ArrContent2)}`}</small><br></br>
         <small>{`Sample Covariance ${ss.sampleCovariance(ArrContent1, ArrContent2)}`}</small><br></br>
         </div>
+        <br></br>
+        <div style={styles}>
+          <Header>The Data!</Header> 
+        {props.fileData.map(i => {
+        //  console.log(i);
+         return (<li>{JSON.stringify(i)}</li>)
+        })}</div>
+        <div> 
+        <br></br>
+      </div>
+      </div>
         
           </Grid.Column>
         </Grid.Row>
